2cc69faf190edc9cc873c89fc049befc86e59e22
Ouail El onsri <elonsriouail1@gmail.com>
2026-05-12 19:36:43 +0100

    dobe

--- internals/00_data_structures/DynamicArray.py
+++ internals/00_data_structures/DynamicArray.py
@@ -5,12 +5,29 @@ import ctypes
 class DynamicArray(object):
 
 	def __init__(self):
-		self._size
-		self._capacity
-		self._array
+		self._size = 0
+		self._capacity = 1
+		self._array = self._make_array(self._capacity)
+
+	def _make_array(self, cap):
+		
+		return (cap * ctypes.py_object)()
+
+	def _resize(self, new_cap):
+		_new_array = self._make_array(new_cap)
+
+		for i in range(self._size):
+			_new_array[i] = self._array[i]
+
+		self._array = _new_array
+		self._capacity = new_cap
 
 	def append(self, val):
-		pass
+		if self._size == self._capacity:
+			self._resize(2 * self._capacity)
+		self._array[self._size] = val
+		self._size += 1
+
 	def insert(self, idx, val):
 		pass
 
@@ -35,19 +52,41 @@ class DynamicArray(object):
 	def copy(self):
 		pass
 	def __getitem__(self, idx):
-		pass
+		if not idx <= self._size:
+			raise IndexError(f"{idx} is out of bound")
+		return self._array[idx]
 
 	def __setitem__(self, idx, val):
 		pass
 
 	def __len__(self):
-		pass
+		return self._size
 
 	def __contains__(self, val):
-		pass
+		if not val in self._array:
+			raise IndexError("sss")
+		return val in self._array
 
 	def __iter__(self):
 		pass
 
 	def __repr__(self):
 		pass
+
+	def __str__(self):
+		pass
+
+
+a = DynamicArray()
+
+print(len(a))
+a.append(888)
+a.append(22)
+print(a[0])
+print(a[1])
+
+print(len(a))
+
+
+
+print(22 in a)
